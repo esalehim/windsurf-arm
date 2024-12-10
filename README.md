6fbac495b4464ad98d5780c5a17ae53335d4aac0
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-12-10 11:20:23 +0100

    Adapted to Windsurf Editor by Codeium

--- README.md
+++ README.md
@@ -1,38 +1,34 @@
 <h1>
     <img src="./logo.png" width="64px" align="center">
-    Cursor for ARM
+    Windsurf for ARM
 </h1>
 
-[Cursor](https://cursor.com) built for Linux and Windows ARM.
-
-The official issue tracker has numerous issues ([#1532](https://github.com/getcursor/cursor/issues/1532) and [#1410](https://github.com/getcursor/cursor/issues/1410)) for official ARM support. Read the [FAQ](#faq) to learn how this works.
+[Windsurf](https://windsurf.codeium.com) built for Linux and Windows ARM.
 
 > [!NOTE]
 > These are unofficial builds.
 
 ## Install
 
-Download the [latest release](https://github.com/coder/cursor-arm/releases/latest) and execute it.
+Download the [latest release](https://github.com/coder/windsurf-arm/releases/latest) and execute it.
 
-For Nix users: `nix run github:coder/cursor-arm` (this should work for all architectures, not just arm).
+For Nix users: `nix run github:codeium/windsurf-arm` (this should work for all architectures, not just arm).
 
 ## FAQ
 
 ### How is this possible?
 
-Cursor is a closed-source fork of VS Code based on Electron that distributes using `AppImage`.
+Windsurf is a fork of VS Code based on Electron that distributes using `.tar.gz` archives. The contents are virtually the same as the built and distributed archives of VS Code.
 
-An `AppImage` is an executable archive that can be extracted with `7z`. The contents are virtually the same as the built and distributed archives of VS Code.
-
-Overwriting the JavaScript for VS Code with Cursor's archive creates a Cursor build.
+Overwriting the JavaScript for VS Code with Windsurf's archive creates a Windsurf build.
 
 ```bash
-cp -R $cursorSrc/resources/app/out $vscodeSrc/resources/app/
+cp -R $windsurfSrc/resources/app/out $vscodeSrc/resources/app/
 ```
 
 See the [build process](./flake.nix#L48) to see exactly how this works.
 
-### Shouldn't Cursor do this?
+### Shouldn't Windsurf do this?
 
 Yes. Hopefully they will!
 
@@ -42,6 +38,6 @@ Seemingly. The [build process](./flake.nix#L48) is quite simple. Try it for your
 
 ### How do updates work?
 
-The Nix flake will be updated as Cursor updates and a new GitHub release will be published.
+The Nix flake will be updated as Windsurf updates and a new GitHub release will be published.
 
 Automatic updates do not work.
--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
-  description = "Flake for building Cursor on Linux ARM and Windows ARM platforms";
+  description = "Flake for building Windsurf on Linux ARM and Windows ARM platforms";
 
   inputs = {
-    # Pinned to align the version of VS Codium with Cursor's build.
-    nixpkgs.url = "github:NixOS/nixpkgs/212defe037698e18fc9521dfe451779a8979844c";
+    # Pinned to align the version of VS Codium with Windsurf's build.
+    nixpkgs.url = "github:NixOS/nixpkgs/823f850f6b269dec3e515ee1c070adc9cb0bcb33";
   };
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "aarch64-linux" "armv7l-linux" "x86_64-linux" ];
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
@@ -16,189 +16,135 @@
         let
           pkgs = import nixpkgs { inherit system; };
 
-          # It's difficult to find pinned versions of Cursor.
-          # The latest version and download URL were found here:
-          # https://changelog.cursor.sh/
-          cursorVersion = "0.43.0";
-          # This is the VS Code version that Cursor was built on.
-          vscodeVersion = "1.93.1";
+          # This is the VS Code version that Windsurf was built on.
+          vscodeVersion = "1.94.0";
 
-          cursorSrc = pkgs.appimageTools.extractType2 {
-            name = "cursor-appimage";
-            src = pkgs.fetchurl {
-              url = "https://dl.todesktop.com/230313mzl4w4u92/versions/${cursorVersion}/linux/appImage/x64";
-              sha256 = "sha256-IcAUXGSMHxGd5Ak4cYA9/2YYg8UA+cRBGgnOupDuRXs=";
-            };
+          # Version of Windsurf being built
+          windsurfVersion = "1.0.7";
+
+          windsurfSrc = builtins.fetchTarball {
+            url = "https://windsurf-stable.codeiumdata.com/linux-x64/stable/bf4345439764c543a1e5ff3517bbce5a22128bca/Windsurf-linux-x64-1.0.7.tar.gz";
+            sha256 = "sha256:1wkbjfxndiyi1sb518g9fm4i55r4i31ma6jrpmyd7dcy4gcjacfi";
           };
           vscodeLinuxArm64 = builtins.fetchTarball {
             url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-arm64/stable";
-            sha256 = "sha256:041bkfbrf1nxq1fr9745h70ky8i3jby8kgihpaf5pwp5cbvzbsnw";
-          };
-          vscodeLinuxArm32 = builtins.fetchTarball {
-            url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-armhf/stable";
-            sha256 = "sha256:19lv6jk54zq8j9khb9ds819jc7x7izvamgkg7sqhaq891kjvvv4a";
+            sha256 = "sha256:0gbl94ai7jdwl31fvsac3mg5lr7f7di6pslbxjwfmsnxs3g2gvcp";
           };
           vscodeWindowsArm64 = pkgs.fetchzip {
             stripRoot = false;
             url = "https://update.code.visualstudio.com/${vscodeVersion}/win32-arm64-archive/stable#file.zip";
-            sha256 = "sha256-vxTGt1qT/bxwb5DXPMxUBrFPuP5xnqXEYWkQMrKEeXI=";
+            sha256 = "sha256-Ybxfv/pb0p5hF3Xul/V0DjbkRDuTt+q+pzK+H61g1i8=";
           };
 
-          # Copies Cursor-specific resources into the VS Code package.
-          copyCursorFiles = root: ''
-            cp -R ${cursorSrc}/resources/app/out ${root}/resources/app/
-            cp -R ${cursorSrc}/resources/app/*.json ${root}/resources/app/
-            cp -R ${cursorSrc}/resources/app/extensions/cursor-* ${root}/resources/app/extensions/
+          # Copies Windsurf-specific resources into the VS Code package.
+          copyWindsurfFiles = root: ''
+            cp -R ${windsurfSrc}/resources/app/out ${root}/resources/app/
+            cp -R ${windsurfSrc}/resources/app/*.json ${root}/resources/app/
+            cp -R ${windsurfSrc}/resources/app/extensions/windsurf-* ${root}/resources/app/extensions/
             rm -rf "${root}/resources/app/node_modules"{,.asar}
-            cp -R ${cursorSrc}/resources/app/node_modules.asar ${root}/resources/app/
+            cp -R ${windsurfSrc}/resources/app/node_modules{,.asar} ${root}/resources/app/
             rm -rf ${root}/resources/app/resources
-            cp -R ${cursorSrc}/resources/app/resources ${root}/resources/app/
+            cp -R ${windsurfSrc}/resources/app/resources ${root}/resources/app/
+            rm -rf ${root}/bin
+            cp -R ${windsurfSrc}/bin ${root}/
           '';
 
-          artifactName = os: arch: ext: "cursor_${cursorVersion}_${os}_${arch}.${ext}";
+          artifactName = os: arch: ext: "windsurf_${windsurfVersion}_${os}_${arch}.${ext}";
 
-          # Overrides the VS Code package with Cursor's resources.
-          buildCursorNix = { vscodePackage }:
+          # Overrides the VS Code package with Windsurf's resources.
+          buildWindsurfNix = { vscodePackage }:
             vscodePackage.overrideAttrs (oldAttrs: {
-              pname = "cursor";
-              version = cursorVersion;
-              executableName = "cursor";
+              pname = "windsurf";
+              version = windsurfVersion;
+              executableName = "windsurf";
 
               postInstall = (oldAttrs.postInstall or "") + ''
-                ${(copyCursorFiles "$out/lib/vscode")}
+                ${(copyWindsurfFiles "$out/lib/vscode")}
 
-                # Replace VS Code's icon and desktop file with Cursor's
-                cp ${cursorSrc}/cursor.png $out/share/pixmaps/cursor.png
+                # Replace VS Code's icon and desktop file with Windsurf's
+                cp ${windsurfSrc}/resources/app/resources/linux/code.png $out/share/pixmaps/windsurf.png
 
                 # Rename the binary
-                mv $out/bin/code $out/bin/cursor || true
-                mv $out/bin/codium $out/bin/cursor || true
+                mv $out/bin/code $out/bin/windsurf || true
+                mv $out/bin/codium $out/bin/windsurf || true
 
                 # Wrap the binary to disable no-new-privileges
-                mv $out/bin/cursor $out/bin/.cursor-wrapped
-                makeWrapper $out/bin/.cursor-wrapped $out/bin/cursor \
+                mv $out/bin/windsurf $out/bin/.windsurf-wrapped
+                makeWrapper $out/bin/.windsurf-wrapped $out/bin/windsurf \
                   --set NO_NEW_PRIVILEGES "0"
               '';
 
               nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
 
               meta = oldAttrs.meta // {
-                description = "Cursor ${cursorVersion}";
-                mainProgram = "cursor";
+                description = "Windsurf ${windsurfVersion}";
+                mainProgram = "windsurf";
               };
             });
 
-          # Builds Cursor using a prepackaged set of VS Code files.
+          # Builds Windsurf using a prepackaged set of VS Code files.
           # This is for creating dynamically linked builds.
-          buildCursorPrepackaged = { vscodeFiles, name, isWindows ? false }:
+          buildWindsurfPrepackaged = { vscodeFiles, name, isWindows ? false }:
             pkgs.stdenv.mkDerivation {
-              pname = "cursor-${name}";
-              version = cursorVersion;
+              pname = "windsurf-${name}";
+              version = windsurfVersion;
               src = vscodeFiles;
               dontPatchShebangs = true;
               buildPhase = ''
                 cp -R --preserve=mode,timestamps . $out
 
-                ${copyCursorFiles "$out"}
+                ${copyWindsurfFiles "$out"}
 
-                # Replace VS Code's icon and desktop file with Cursor's
-                cp ${cursorSrc}/cursor.png $out/cursor.png
-                cp ${cursorSrc}/cursor.desktop $out/cursor.desktop
-                cp -R ${cursorSrc}/resources/todesktop* $out/resources/
-                
-                # This is excluded intentionally. It causes an error in console,
-                # but there's some token we must be missing?
-                # cp ${cursorSrc}/resources/app-update.yml $out/resources/
+                # Replace VS Code's icon and desktop file with Windsurf's
+                #cp ${windsurfSrc}/resources/app/resources/linux/code.png $out/windsurf.png
+                #cp ${windsurfSrc}/windsurf.desktop $out/windsurf.desktop
+                #cp -R ${windsurfSrc}/resources/todesktop* $out/resources/
 
-                # Cursor doesn't have a root bin dir
-                rm -rf $out/bin
+                mv $out/code $out/windsurf || true
+                mv $out/codium $out/windsurf || true
+                mv $out/Code.exe $out/Windsurf.exe || true
 
-                mv $out/code $out/cursor || true
-                mv $out/codium $out/cursor || true
-                mv $out/Code.exe $out/Cursor.exe || true
-
-                # Platform-specific adjustments
-                ${if !isWindows then ''
-                  cp -R ${cursorSrc}/usr $out
-                  cp ${cursorSrc}/AppRun $out
-                  cp ${cursorSrc}/.DirIcon $out
-                '' else ''
-                ''}
               '';
             };
         in
         {
           extract-vscode-version = pkgs.runCommand "extract-vscode-version" { } ''
-            ${pkgs.jq}/bin/jq -r .vscodeVersion ${cursorSrc}/resources/app/product.json >> $out
+            ${pkgs.jq}/bin/jq -r .vscodeVersion ${windsurfSrc}/resources/app/product.json >> $out
           '';
 
-          cursor = {
-            unpacked = cursorSrc;
+          windsurf = {
+            unpacked = windsurfSrc;
 
             linux = {
-              arm64 = buildCursorPrepackaged {
+              arm64 = buildWindsurfPrepackaged {
                 vscodeFiles = vscodeLinuxArm64;
                 name = "linux-arm64";
               };
-              arm64-appimage = pkgs.stdenv.mkDerivation {
-                name = artifactName "linux" "arm64" "AppImage";
-                src = self.packages.${system}.cursor.linux.arm64;
-                dontPatchShebangs = true;
-                buildInputs = [ pkgs.appimagekit ];
-                buildPhase = ''
-                  mkdir -p $out
-                  ARCH=arm_aarch64 appimagetool $src $out/${artifactName "linux" "arm64" "AppImage"}
-                  chmod +x $out/${artifactName "linux" "arm64" "AppImage"}
-                  patchelf --set-interpreter /lib/ld-linux-aarch64.so.1 $out/${artifactName "linux" "arm64" "AppImage"}
-                '';
-              };
               arm64-targz = pkgs.runCommand "tarball" { } ''
                 mkdir -p $out
-                cd ${self.packages.${system}.cursor.linux.arm64}
+                cd ${self.packages.${system}.windsurf.linux.arm64}
                 ${pkgs.gnutar}/bin/tar -czvf $out/${artifactName "linux" "arm64" "tar.gz"} .
-              '';
-
-              arm32 = buildCursorPrepackaged {
-                vscodeFiles = vscodeLinuxArm32;
-                name = "linux-arm32";
-              };
-              arm32-appimage = pkgs.stdenv.mkDerivation {
-                name = artifactName "linux" "arm32" "AppImage";
-                src = self.packages.${system}.cursor.linux.arm32;
-                dontPatchShebangs = true;
-                buildInputs = [ pkgs.appimagekit ];
-                buildPhase = ''
-                  mkdir -p $out
-                  ARCH=arm appimagetool $src $out/${artifactName "linux" "arm32" "AppImage"}
-                  chmod +x $out/${artifactName "linux" "arm32" "AppImage"}
-                  patchelf --set-interpreter /lib/ld-linux.so.3 $out/${artifactName "linux" "arm32" "AppImage"}
-                '';
-              };
-              arm32-targz = pkgs.runCommand "tarball" { } ''
-                mkdir -p $out
-                cd ${self.packages.${system}.cursor.linux.arm32}
-                ${pkgs.gnutar}/bin/tar -czvf $out/${artifactName "linux" "arm32" "tar.gz"} .
               '';
             };
             windows = {
-              arm64 = buildCursorPrepackaged {
+              arm64 = buildWindsurfPrepackaged {
                 vscodeFiles = vscodeWindowsArm64;
                 name = "windows-arm64";
                 isWindows = true;
               };
               arm64-zip = pkgs.runCommand "zip" { } ''
                 mkdir -p $out
-                cd ${self.packages.${system}.cursor.windows.arm64}
+                cd ${self.packages.${system}.windsurf.windows.arm64}
                 ${pkgs.zip}/bin/zip -r $out/${artifactName "windows" "arm64" "zip"} .
               '';
             };
 
-            nix = buildCursorNix {
+            nix = buildWindsurfNix {
               vscodePackage = pkgs.vscodium;
             };
           };
 
-          default = self.packages.${system}.cursor.nix;
+          default = self.packages.${system}.windsurf.nix;
         }
       );
     };
